32aeceba587569619870e5fae9aba4bb949d6e9a
Harshit_Parmar <parmarharshit441@gmail.com>
2025-11-24 20:20:24 +0530

    I Think I Have to Revise Web Dev 🥴

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const isProduction = process.env.VITE_ENV === "production";
-const productionURL = process.env.VITE_PROD_API_URL;
+const isProduction = import.meta.env.VITE_ENV === "production";
+const productionURL = import.meta.env.VITE_PROD_API_URL;
 
 export const apiClient = axios.create({
   baseURL: isProduction ? productionURL : "http://localhost:5000/api",
